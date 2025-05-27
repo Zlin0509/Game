580e48185d627449dbbf37374746c3972100fc98
zzz <zlin0509@>
2025-05-27 22:11:38 +0800

    daily

---
+++ headers/Game.h
@@ -0,0 +1,8 @@
+//
+// Created by 27682 on 2025/5/27.
+//
+
+#ifndef GAME_H
+#define GAME_H
+
+#endif //GAME_H
---
+++ headers/Player.h
@@ -0,0 +1,8 @@
+//
+// Created by 27682 on 2025/5/27.
+//
+
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#endif //PLAYER_H
